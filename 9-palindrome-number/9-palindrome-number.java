d79e95380d308528c750a6edaabc980cad029fdc
suhaspuli-11 <102015184+suhaspuli-11@users.noreply.github.com>
2022-08-07 07:49:40 +0530

    Time: 15 ms (51.69%), Space: 46.1 MB (15.78%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,7 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s1 = Integer.toString(x);
-        StringBuilder s2 = new StringBuilder(s1).reverse();
-        return (s1.equals(s2.toString()));
+        // Two pointer method
+        String s = Integer.toString(x);
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))
+                return false;
+            i += 1;
+            j -= 1;
+        }
+        return true;
     }
 }
