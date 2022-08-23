1f031b448c5a92db5abcdb8e3b4ea07ad2a7342e
suhaspuli-11 <102015184+suhaspuli-11@users.noreply.github.com>
2022-08-23 17:39:03 +0530

    Time: 2 ms (83.15%), Space: 42.1 MB (73.68%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -3,10 +3,8 @@ class Solution {
         int s1=0,s2=0;
         for(int i=0;i<s.length();i++)
             s1 += s.charAt(i);
-        System.out.println(s1);
         for(int i=0;i<t.length();i++)
             s2 += t.charAt(i);
-        System.out.println(s2);
         int ans=s2-s1;
         return (char)ans;
     }
