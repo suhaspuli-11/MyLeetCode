bc1260415ea271b8344ed2752b2f42a91f7e9216
suhaspuli-11 <102015184+suhaspuli-11@users.noreply.github.com>
2022-08-26 12:02:46 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (47.86%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        if(num==0) return true;
-        if(num%10==0) return false;
-        return true;
+        return num==0||num%10!=0;
     }
 }
